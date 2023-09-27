8e9b5340f656dfd3b68b05ef7d46fb00285ef819
BhavyaDivecha <93636310+BhavyaDivecha@users.noreply.github.com>
2023-09-27 15:21:10 -0600

    commit trial

---
+++ inventoryManagement.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.8;
+
+contract inventoryManagement {
+  struct Inventory {
+        uint256 p_ID;
+        uint256 quantity;
+        string p_Name;
+        bool exists;
+    }
+
+    mapping (uint256 => Inventory) public inventories;
+
+    function addInventory(uint256 p_ID, uint256 quantity, string memory p_Name) public {
+        require(!inventories[p_ID].exists, "Inventory already exists");
+        inventories[p_ID] = Inventory(p_ID, quantity, p_Name, true);
+    }
+
+    function updateInventory(uint256 p_ID, uint256 quantity, string memory p_Name) public {
+        require(inventories[p_ID].exists, "Inventory does not exist");
+        inventories[p_ID].quantity = quantity;
+        inventories[p_ID].p_Name = p_Name;
+    }
+
+    function getInventory(uint256 p_ID) public view returns (string memory, uint256) {
+        require(inventories[p_ID].exists, "Inventory does not exist");
+        return (inventories[p_ID].p_Name, inventories[p_ID].quantity);
+    }
+
+    function removeInventory(uint256 p_ID) public {
+        require(inventories[p_ID].exists, "Inventory does not exist");
+        delete inventories[p_ID];
+    }
+}
+ 
+
+
